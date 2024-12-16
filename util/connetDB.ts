07313e5693aff5c3089c00a22041b8eca5173dea
shounak03 <shounakmis.3@gmail.com>
2024-12-16 21:25:07 +0530

    trying to fix /message route, try 1

--- util/connetDB.ts
+++ util/connetDB.ts
@@ -1,30 +1,47 @@
-import { MongoClient, ServerApiVersion } from "mongodb";
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
-type ConnectionObj = {
-    isConnected?: number;
+interface GlobalMongoose {
+  conn: typeof mongoose | null;
+  promise: Promise<typeof mongoose> | null;
 }
 
-const connection: ConnectionObj = {}
+declare global {
+  var mongoose: GlobalMongoose | undefined;
+}
 
-if (!process.env.MONGODB_URI) {
-    throw new Error('Invalid/Missing environment variable: "MONGODB_URI"')
+const MONGODB_URI = process.env.MONGODB_URI!;
+
+if (!MONGODB_URI) {
+  throw new Error('Please define the MONGODB_URI environment variable');
+}
+
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = {
+    conn: null,
+    promise: null,
+  };
+}
+
+export async function connectDB(): Promise<typeof mongoose> {
+  if (cached?.conn) {
+    return cached.conn;
   }
-   
-const uri = process.env.MONGODB_URI
 
-export const connectDB = async ():Promise<void> => {
-    if(connection.isConnected ) {
-        console.log("Database already connected");
-        return
-    }
-    try {
-        const conn = await mongoose.connect(uri);
-        connection.isConnected = conn.connections[0].readyState
-        console.log("Database connected successfully");
-    }
-    catch (error) {
-        console.log("Connection failed - ",error)
-        process.exit()
-    }
+  if (!cached?.promise) {
+    const opts = {
+      bufferCommands: false,
+    };
+
+    cached!.promise = mongoose.connect(MONGODB_URI, opts);
+  }
+
+  try {
+    cached!.conn = await cached!.promise;
+    return cached!.conn;
+  } catch (e) {
+    cached!.promise = null;
+    throw e;
+  }
 }
