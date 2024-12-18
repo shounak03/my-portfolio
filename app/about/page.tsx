1108a054ae5a09a5322acdd8f519ff09009dffc7
shounak03 <shounakmis.3@gmail.com>
2024-12-18 22:54:50 +0530

    minor changes

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -6,11 +6,11 @@ export default function AboutSection() {
       <div className="container mx-auto px-4">
         <h2 className="text-3xl font-bold text-primary mb-8 text-center">About Me</h2>
         <div className="flex flex-col items-center space-y-8">
-          <Card className="border-2 border-primary bg-white w-full max-w-2xl">
+          <Card className="border-2 border-primary bg-white w-full max-w-2xl dark:bg-background text-white">
             <CardHeader>
               <CardTitle className="text-xl font-semibold text-primary">Education</CardTitle>
             </CardHeader>
-            <CardContent className="text-foreground max-h-64 overflow-y-auto dark:text-black">
+            <CardContent className="text-foreground max-h-64 overflow-y-auto ">
               <p className="text-xl ">
                 I hold a Bachelor&apos;s degree in Computer Science from a Kalinga institute of industrial technology
                 University, where I honed my technical skills and gained a deep
@@ -19,12 +19,12 @@ export default function AboutSection() {
             </CardContent>
           </Card>
 
-          <Card className="border-2 border-primary bg-white w-full max-w-2xl">
+          <Card className="border-2 border-primary bg-white w-full max-w-2xl dark:bg-background text-white">
             <CardHeader>
               <CardTitle className="text-xl font-semibold text-primary">Tech Stack</CardTitle>
             </CardHeader>
             <CardContent className="text-foreground max-h-64 overflow-y-auto">
-                <ul className="list-disc list-inside space-y-2 dark:text-black">
+                <ul className="list-disc list-inside space-y-2">
                     <li><span className="text-xl font-bold">Languages</span>: <span className="text-xl">JavaScript, TypeScript, Python, Solidity</span> </li>
                     <li><span className="text-xl font-bold">Frontend</span>: <span className="text-xl">React, Next, Tailwind, UI</span> </li>
                     <li><span className="text-xl font-bold">Backend</span>:<span className="text-xl">  Node, Express, Nest, fastapi </span></li>
@@ -36,12 +36,12 @@ export default function AboutSection() {
 
           </Card>
 
-          <Card className="border-2 border-primary bg-white w-full max-w-2xl">
+          <Card className="border-2 border-primary bg-white w-full max-w-2xl dark:bg-background text-white">
             <CardHeader>
               <CardTitle className="text-xl font-semibold text-primary">Experience</CardTitle>
             </CardHeader>
             <CardContent className="text-foreground max-h-64 overflow-y-auto">
-              <ul className="list-disc list-inside space-y-2 dark:text-black">
+              <ul className="list-disc list-inside space-y-2 ">
 
                 <li><span className="text-xl">Freelance Web Developer, 2024-present</span></li>
                 <li><span className="text-xl">Web Development Intern, Reluhash.Ai, Summer 2024</span></li>
